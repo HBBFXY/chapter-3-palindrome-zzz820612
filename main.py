308d310f350762f7ef72ebd0b3728df1cfa58f7f
zzz820612 <zyz198312@qq.com>
2025-09-18 18:00:49 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-# 这里编写你的代码
+def main():
+    num = input().strip()  # 读取输入并去除首尾空格
+    # 判断输入是否为纯数字，且长度为5
+    if not num.isdigit() or len(num) != 5:
+        print("输入错误: 请输入5位数字")
+    else:
+        if num == num[::-1]:
+            print("是回文数")
+        else:
+            print("不是回文数")
+if __name__ == "__main__":
+    main()
